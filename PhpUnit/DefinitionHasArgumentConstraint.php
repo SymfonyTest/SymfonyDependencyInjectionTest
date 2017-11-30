f582683338f4fb448a5dcca3b7687fe45700d5ce
Tony Lemke <tlemke@naji-dev.de>
2017-11-30 11:15:44 +0100

    Support for named arguments (#79)
    * Support for named arguments
    
    Since 3.3, Symfony supports named arguments. Therefore casting the argumentIndex to an integer does not support all cases anymore.
    
    * Better wording for named arguments.
    
    * Throws an exception, if the argument is a named but the empty string
    
    * test fixed

--- PhpUnit/DefinitionHasArgumentConstraint.php
+++ PhpUnit/DefinitionHasArgumentConstraint.php
@@ -9,6 +9,10 @@ use Symfony\Component\DependencyInjection\Exception\OutOfBoundsException;
 
 class DefinitionHasArgumentConstraint extends Constraint
 {
+
+    /**
+     * @var int|string
+     */
     private $argumentIndex;
     private $expectedValue;
     private $checkExpectedValue;
@@ -17,13 +21,36 @@ class DefinitionHasArgumentConstraint extends Constraint
     {
         parent::__construct();
 
-        $this->argumentIndex = (integer)$argumentIndex;
+        if (!(is_string($argumentIndex) || (is_int($argumentIndex) && $argumentIndex >= 0))) {
+            throw new \InvalidArgumentException('Expected either a string or a positive integer for $argumentIndex.');
+        }
+
+        if (is_string($argumentIndex)) {
+            if ('' === $argumentIndex) {
+                throw new \InvalidArgumentException('A named argument must begin with a "$".');
+            }
+
+            if ('$' !== $argumentIndex[0]) {
+                throw new \InvalidArgumentException(
+                    sprintf('Unknown argument "%s". Did you mean "$%s"?', $argumentIndex, $argumentIndex)
+                );
+            }
+        }
+
+        $this->argumentIndex = $argumentIndex;
         $this->expectedValue = $expectedValue;
         $this->checkExpectedValue = $checkExpectedValue;
     }
 
     public function toString()
     {
+        if (is_string($this->argumentIndex)) {
+            return sprintf(
+                'has an argument named "%s" with the given value',
+                $this->argumentIndex
+            );
+        }
+
         return sprintf(
             'has an argument with index %d with the given value',
             $this->argumentIndex
@@ -65,13 +92,13 @@ class DefinitionHasArgumentConstraint extends Constraint
                 return false;
             }
 
-            $this->fail(
-                $definition,
-                sprintf(
-                    'The definition has no argument with index %d',
-                    $this->argumentIndex
-                )
-            );
+            if (is_string($this->argumentIndex)) {
+                $message = sprintf('The definition has no argument named "%s"', $this->argumentIndex);
+            } else {
+                $message = sprintf('The definition has no argument with index %d', $this->argumentIndex);
+            }
+
+            $this->fail($definition, $message);
         }
 
         return true;
@@ -88,15 +115,23 @@ class DefinitionHasArgumentConstraint extends Constraint
                 return false;
             }
 
-            $this->fail(
-                $definition,
-                sprintf(
+            if (is_string($this->argumentIndex)) {
+                $message = sprintf(
+                    'The value of argument named "%s" (%s) is not equal to the expected value (%s)',
+                    $this->argumentIndex,
+                    $this->exporter->export($actualValue),
+                    $this->exporter->export($this->expectedValue)
+                );
+            } else {
+                $message = sprintf(
                     'The value of argument with index %d (%s) is not equal to the expected value (%s)',
                     $this->argumentIndex,
                     $this->exporter->export($actualValue),
                     $this->exporter->export($this->expectedValue)
-                )
-            );
+                );
+            }
+
+            $this->fail($definition, $message);
         }
 
         return true;
